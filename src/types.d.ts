23a1ff2622efd8ad990cd47762ebb1b66c72684a
Damet24 <damet.2401@gmail.com>
2023-03-01 18:10:52 -0500

    types

---
+++ src/types.d.ts
@@ -0,0 +1,29 @@
+
+export interface Info {
+    count: number;
+    pages: number;
+    next:  string;
+    prev:  null;
+}
+
+export enum Gender {
+    Female = "Female",
+    Male = "Male",
+    Unknown = "unknown",
+}
+
+export interface Location {
+    name: string;
+    url:  string;
+}
+
+export enum Species {
+    Alien = "Alien",
+    Human = "Human",
+}
+
+export enum Status {
+    Alive = "Alive",
+    Dead = "Dead",
+    Unknown = "unknown",
+}

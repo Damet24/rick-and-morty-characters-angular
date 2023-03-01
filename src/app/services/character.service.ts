715fec4eecceca8dc40a64ac7541b05d15a414f9
Damet24 <damet.2401@gmail.com>
2023-03-01 18:10:39 -0500

    character service

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CharacterData } from '../models/character.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllCharacters() {
+    return this.http.get<CharacterData>('https://rickandmortyapi.com/api/character')
+  }
 }

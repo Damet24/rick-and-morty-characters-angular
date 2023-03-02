20bfdc57085e8981cf286f8bf5dd4f3d91222247
Damet24 <damet.2401@gmail.com>
2023-03-02 15:19:31 -0500

    styles fixed

--- src/app/components/toggle-buttom/toggle-buttom.component.ts
+++ src/app/components/toggle-buttom/toggle-buttom.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-toggle-buttom',
   templateUrl: './toggle-buttom.component.html',
   styleUrls: ['./toggle-buttom.component.scss']
 })
-export class ToggleButtomComponent {
-  check = false
+export class ToggleButtomComponent implements OnInit {
+  @Input() check = false
 
-  a() {
-    console.log(this.check)
+  ngOnInit() {
+    const theme = window.localStorage.getItem('theme')
+    if (theme) {
+      this.check = theme === 'dark'
+      if (this.check) document.getElementsByTagName('html')[0].classList.add('dark')
+    }
+    else {
+      window.localStorage.setItem('theme', 'light')
+      document.getElementsByTagName('html')[0].classList.remove('dark')
+    }
+  }
+
+  onToggle() {
+    if (this.check) {
+      document.getElementsByTagName('html')[0].classList.remove('dark')
+      window.localStorage.setItem('theme', 'light')
+    }
+    else {
+      document.getElementsByTagName('html')[0].classList.add('dark')
+      window.localStorage.setItem('theme', 'dark')
+    }
   }
 }

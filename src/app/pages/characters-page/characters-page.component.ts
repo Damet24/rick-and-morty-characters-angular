01eb50d0f2b0a14f3656e623836319e10ed2ce03
Damet24 <damet.2401@gmail.com>
2023-03-02 15:19:44 -0500

    styles fixed

--- src/app/pages/characters-page/characters-page.component.ts
+++ src/app/pages/characters-page/characters-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Character } from 'src/app/models/character.model';
 import { CharacterService } from 'src/app/services/character.service';
+import { Info } from '../../../types.d'
 
 @Component({
   selector: 'app-characters-page',
@@ -10,20 +12,35 @@ import { CharacterService } from 'src/app/services/character.service';
 export class CharactersPageComponent implements OnInit {
 
   characterName: string = ''
+  info: Info = {
+    count: 0,
+    pages: 0,
+    next: '',
+    prev: null
+  }
   characters: Character[] = []
+  page: string | null = null
 
-  constructor(private characterService: CharacterService) { }
+  constructor(private characterService: CharacterService,
+  private router: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.characterService.getAllCharacters()
       .subscribe(data => {
+        this.info = data.info
         this.characters = data.results
       })
+
+    this.router.queryParamMap.subscribe(params => {
+      this.page = params.get('page')
+      this.searchCharacter()
+    })
   }
 
   searchCharacter() {
-    this.characterService.getAllCharacters(this.characterName)
+    this.characterService.getAllCharacters(this.characterName, this.page ?? '1')
       .subscribe(data => {
+        this.info = data.info
         this.characters = data.results
       })
   }

1f3b079e64d5a1f42e35d6a8f45d41af40b54598
Damet24 <damet.2401@gmail.com>
2023-03-02 08:07:17 -0500

    render all characters

--- src/app/pages/characters-page/characters-page.component.ts
+++ src/app/pages/characters-page/characters-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Character } from 'src/app/models/character.model';
+import { CharacterService } from 'src/app/services/character.service';
 
 @Component({
   selector: 'app-characters-page',
   templateUrl: './characters-page.component.html',
   styleUrls: ['./characters-page.component.scss']
 })
-export class CharactersPageComponent {
+export class CharactersPageComponent implements OnInit {
 
+  characters: Character[] = []
+
+  constructor(private characterService: CharacterService) { }
+
+  ngOnInit(): void {
+    this.characterService.getAllCharacters()
+      .subscribe(data => {
+        console.log(data);
+        this.characters = data.results
+      })
+  }
 }

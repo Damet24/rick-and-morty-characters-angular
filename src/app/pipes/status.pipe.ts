de39923f69dad3a9902cb873bbbb4783496c7dd6
Damet24 <damet.2401@gmail.com>
2023-03-02 10:30:38 -0500

    status pipe

---
+++ src/app/pipes/status.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'status'
+})
+export class StatusPipe implements PipeTransform {
+
+  transform(value: string = ''): string {
+    const default_class = 'bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500'
+
+    switch (value) {
+      case 'Unknown':
+        return default_class
+      case 'Alive':
+        return 'bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400'
+      case 'Dead':
+        return 'bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400'
+
+      default:
+        return default_class;
+    }
+  }
+
+}

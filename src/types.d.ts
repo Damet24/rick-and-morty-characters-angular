7b92fd2bb5ccf7fdde8b91d795bf1b17e67021d1
Damet24 <damet.2401@gmail.com>
2023-03-01 20:00:01 -0500

    types farmating

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,29 +1,29 @@
 
 export interface Info {
-    count: number;
-    pages: number;
-    next:  string;
-    prev:  null;
+  count: number;
+  pages: number;
+  next: string;
+  prev: null;
 }
 
 export enum Gender {
-    Female = "Female",
-    Male = "Male",
-    Unknown = "unknown",
+  Female = "Female",
+  Male = "Male",
+  Unknown = "unknown",
 }
 
 export interface Location {
-    name: string;
-    url:  string;
+  name: string;
+  url: string;
 }
 
 export enum Species {
-    Alien = "Alien",
-    Human = "Human",
+  Alien = "Alien",
+  Human = "Human",
 }
 
 export enum Status {
-    Alive = "Alive",
-    Dead = "Dead",
-    Unknown = "unknown",
+  Alive = "Alive",
+  Dead = "Dead",
+  Unknown = "unknown",
 }

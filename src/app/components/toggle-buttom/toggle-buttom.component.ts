2d0f7d76d3ce451f861d6a3115c5f87122472e69
Damet24 <damet.2401@gmail.com>
2023-03-08 14:08:48 -0500

    refactor

--- src/app/components/toggle-buttom/toggle-buttom.component.ts
+++ src/app/components/toggle-buttom/toggle-buttom.component.ts
@@ -1,33 +1,37 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { DOCUMENT } from "@angular/common";
+import { Component, Inject, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-toggle-buttom',
-  templateUrl: './toggle-buttom.component.html',
-  styleUrls: ['./toggle-buttom.component.scss']
+  selector: "app-toggle-buttom",
+  templateUrl: "./toggle-buttom.component.html",
+  styleUrls: ["./toggle-buttom.component.scss"],
 })
 export class ToggleButtomComponent implements OnInit {
-  @Input() check = false
+  @Input()
+  check = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
   ngOnInit() {
-    const theme = window.localStorage.getItem('theme')
+    const theme = window.localStorage.getItem("theme");
     if (theme) {
-      this.check = theme === 'dark'
-      if (this.check) document.getElementsByTagName('html')[0].classList.add('dark')
-    }
-    else {
-      window.localStorage.setItem('theme', 'light')
-      document.getElementsByTagName('html')[0].classList.remove('dark')
+      this.check = theme === "dark";
+      if (this.check) {
+        this.document.getElementsByTagName("html")[0].classList.add("dark");
+      }
+    } else {
+      window.localStorage.setItem("theme", "light");
+      this.document.getElementsByTagName("html")[0].classList.remove("dark");
     }
   }
 
   onToggle() {
     if (this.check) {
-      document.getElementsByTagName('html')[0].classList.remove('dark')
-      window.localStorage.setItem('theme', 'light')
-    }
-    else {
-      document.getElementsByTagName('html')[0].classList.add('dark')
-      window.localStorage.setItem('theme', 'dark')
+      this.document.getElementsByTagName("html")[0].classList.remove("dark");
+      window.localStorage.setItem("theme", "light");
+    } else {
+      this.document.getElementsByTagName("html")[0].classList.add("dark");
+      window.localStorage.setItem("theme", "dark");
     }
   }
 }
--- src/app/pages/characters-page/characters-page.component.ts
+++ src/app/pages/characters-page/characters-page.component.ts
@@ -1,74 +1,73 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, HostListener, Inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Character } from 'src/app/models/character.model';
-import { CharacterService } from 'src/app/services/character.service';
-import { Info } from '../../../types.d'
+import { DOCUMENT } from "@angular/common";
+import { Component, HostListener, Inject, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Character } from "src/app/models/character.model";
+import { CharacterService } from "src/app/services/character.service";
+import { Info } from "../../../types.d";
 
 @Component({
-  selector: 'app-characters-page',
-  templateUrl: './characters-page.component.html',
-  styleUrls: ['./characters-page.component.scss']
+  selector: "app-characters-page",
+  templateUrl: "./characters-page.component.html",
+  styleUrls: ["./characters-page.component.scss"],
 })
 export class CharactersPageComponent implements OnInit {
-
-  characterName = ''
+  characterName = "";
   info: Info = {
     count: 0,
     pages: 0,
-    next: '',
-    prev: null
-  }
-  characters: Character[] = []
-  page: string | null = null
-  showButton = false
+    next: "",
+    prev: null,
+  };
+  characters: Character[] = [];
+  page: string | null = null;
+  showButton = false;
 
-  private scroolHight = 500
+  private scroolHight = 500;
 
   constructor(
     private characterService: CharacterService,
     private router: ActivatedRoute,
-    @Inject(DOCUMENT) private document: Document
-  ) { }
+    @Inject(DOCUMENT) private document: Document,
+  ) {}
 
-  @HostListener('window:scroll')
+  @HostListener("window:scroll")
   onWindowScroll() {
-    const yOffset = window.pageXOffset
-    const scrollTop = this.document.documentElement.scrollTop
-    this.showButton = (yOffset || scrollTop) > this.scroolHight
+    const yOffset = window.pageXOffset;
+    const scrollTop = this.document.documentElement.scrollTop;
+    this.showButton = (yOffset || scrollTop) > this.scroolHight;
   }
 
   scrollTop() {
-    this.document.documentElement.scrollTop = 0
+    this.document.documentElement.scrollTop = 0;
   }
 
   ngOnInit(): void {
     this.characterService.getAllCharacters()
-      .subscribe(data => {
-        this.info = data.info
-        this.characters = data.results
-      })
+      .subscribe((data) => {
+        this.info = data.info;
+        this.characters = data.results;
+      });
 
-    this.router.queryParamMap.subscribe(params => {
-      this.page = params.get('page')
-      this.searchCharacter()
-    })
+    this.router.queryParamMap.subscribe((params) => {
+      this.page = params.get("page");
+      this.searchCharacter();
+    });
   }
 
   onScrollEnd() {
-    console.log('mas characters');
-    this.characterService.getAllCharacters('', this.info.next)
-      .subscribe(data => {
-        this.info = data.info
-        this.characters = [...this.characters, ...data.results]
-      })
+    console.log("mas characters");
+    this.characterService.getAllCharacters("", this.info.next)
+      .subscribe((data) => {
+        this.info = data.info;
+        this.characters = [...this.characters, ...data.results];
+      });
   }
 
   searchCharacter() {
     this.characterService.getAllCharacters(this.characterName)
-      .subscribe(data => {
-        this.info = data.info
-        this.characters = data.results
-      })
+      .subscribe((data) => {
+        this.info = data.info;
+        this.characters = data.results;
+      });
   }
 }
